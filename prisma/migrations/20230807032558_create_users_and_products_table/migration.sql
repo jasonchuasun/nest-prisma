822a531fb1e6cafa6f7eadbd3b1b71ff336fa447
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2023-08-07 11:28:24 +0800

    db: reset prisma migration

--- prisma/migrations/20230807032558_create_users_and_products_table/migration.sql
+++ prisma/migrations/20230807032558_create_users_and_products_table/migration.sql
@@ -5,7 +5,9 @@ CREATE TABLE `User` (
     `last_name` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NULL,
+    `role` ENUM('user', 'admin', 'ambassador') NOT NULL DEFAULT 'user',
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
